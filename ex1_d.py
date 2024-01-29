b8ba7b406b2be374dfc4ba7c1910198ed95749e6
LauritsFog <lause.fog@gmail.com>
2024-01-29 17:27:43 +0100

    Update ex1_d.py

--- ex1_d.py
+++ ex1_d.py
@@ -1,16 +1,35 @@
 import math
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def u(x):
     return math.exp(math.cos(x))
 
 
-d2u_1 = 0
-d2u_2 = 0
+def d2u(x):
+    return -math.cos(x)*math.exp(math.cos(x))+math.sin(x)*math.exp(math.cos(x))
 
-for i in range(0, 10):
+n = 10
 
-    h = 1/(2**i)
+err1 = [0 for i in range(n)]
+err2 = [0 for i in range(n)]
+
+H = np.zeros(n)
+refcon4 = np.zeros(n)
+refcon3 = np.zeros(n)
+
+for j in range(n):
+    
+    d2u_1 = 0
+    d2u_2 = 0
+    
+    h = 1/(2**j)
+    H[j] = h
+    
+    # Order 3 and 4 convergence
+    refcon3[j] = h**3
+    refcon4[j] = h**4
 
     # (4,0)
     a1 = [11/(12*h**2), -14/(3*h**2), 19/(2*h**2), -26/(3*h**2), 35/(12*h**2)]
@@ -23,4 +42,14 @@ for i in range(0, 10):
     for i in range(-2, 3):
         d2u_2 += u(i*h)*a2[i+2]
 
-    err1 = u(0) - d2u_1
+    err1[j] = abs(d2u(0) - d2u_1)
+
+    err2[j] = abs(d2u(0) - d2u_2)
+
+plt.loglog(H, err1,'-o')
+plt.loglog(H, err2,'-o')
+plt.loglog(H,refcon4,'--')
+plt.legend(["(4,0)","(2,2)","$h^2$"])
+plt.xlabel("h")
+plt.ylabel("$||e||$")
+plt.show()
