f48b7d408efc17acf2bb8ad3b21dbfabe7314cfc
Marianadesamadsen <s201205@student.dtu.dk>
2024-02-05 09:48:31 +0100

    new

--- ex1_c_mariana.py
+++ ex1_c_mariana.py
@@ -11,7 +11,6 @@ def u(x):
 
 exact_value = -math.exp(1)
 
-print(exact_value)
 def d2u_1(h):
     # (4,0)
     a1 = [11/(12*h**2), -14/(3*h**2), 19/(2*h**2), -26/(3*h**2), 35/(12*h**2)]
@@ -49,19 +48,49 @@ for i in range(range_s):
     error1.append(abs(exact_value - d2u_1(h_list[i])))
     error2.append(abs(exact_value - d2u_2(h_list[i])))
 
+# Convergence rates 
+CR_1 = 4
+CR_2 = 4
+
 # Making subplot
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 10))
 #ax1.loglog(h_list, error1, "-o")
 ax1.plot(np.log10(h_list),np.log10(error1),"-o")
-ax1.set_title('Backward')  
+ax1.plot(np.log10(h_list),CR_1*np.log10(h_list))
+ax1.set_title('Backward operator')  
 ax1.set_xlabel('h')
 ax1.set_ylabel('d2u(h)') 
 #ax2.loglog(h_list, error2, "-o")
 ax2.plot(np.log10(h_list),np.log10(error2),"-o")
-ax2.set_title('Centered')
+ax2.plot(np.log10(h_list),CR_2*np.log10(h_list))
+ax2.set_title('Centered operator')
 ax2.set_xlabel('h')
 ax2.set_ylabel('d2u(h)')  
 
 plt.tight_layout()
 
-plt.show()
+#plt.show()
+
+#%% Problem e
+
+a_forward = [35/12, -104/12, 114/12,-56/12, 11/12]
+a_backward = [11/12,-56/12,114/12,-104/12,35/12]
+
+h = 1
+
+def C(n,a,alpha,beta):
+    out = 0
+    
+    if n == 2:
+        out -= h**(-n)
+    
+    for i,m in enumerate(range(-alpha,beta+1)):
+        
+        out += a[i]*m**n/(math.factorial(n))
+
+    return out 
+
+print([C(i,a_forward,0,4) for i in range(7)])
+print([C(i,a_backward,4,0) for i in range(7)])
+
+
