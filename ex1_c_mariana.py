0723649189a8a4f4c68fbb12cb2367126ecc3e49
Marianadesamadsen <s201205@student.dtu.dk>
2024-02-05 11:39:20 +0100

    order

--- ex1_c_mariana.py
+++ ex1_c_mariana.py
@@ -8,70 +8,38 @@ import numpy as np
 #%% Exercise c
 def u(x):
     return math.exp(math.cos(x))
+    #return math.exp(x)
 
+#exact_value = math.exp(0)
 exact_value = -math.exp(1)
 
-def d2u_1(h):
+def d2u_b(h):
     # (4,0)
-    a1 = [11/(12*h**2), -14/(3*h**2), 19/(2*h**2), -26/(3*h**2), 35/(12*h**2)]
+    x=0
+    #a1 = [11/(12*h**2), -14/(3*h**2), 19/(2*h**2), -26/(3*h**2), 35/(12*h**2)]
+    a1 = [11/(12*h**2), -56/(12*h**2), 114/(12*h**2), -104/(12*h**2), 35/(12*h**2)]
     d2u_1 = 0
-    for i in range(-4, 1):
-        d2u_1 += u(i*h)*a1[i+4]
+    for i,a in zip(range(-4, 1),a1):
+        d2u_1 += u(x+i*h)*a
     
     return d2u_1
 
-def d2u_2(h):
+def d2u_c(h):
     # (2,2)
-    a2 = [-1/(12*h**2), 4/(3*h**2), -5/(2*h**2), 4/(3*h**2), -1/(12*h**2)]
+    x=0
+    #a2 = [-1/(12*h**2), 4/(3*h**2), -5/(2*h**2), 4/(3*h**2), -1/(12*h**2)]
+    a2 = np.array([-1/12,16/12,-30/12,16/12,-1/12])/(h**2)
     d2u_2 = 0
-    for i in range(-2, 3):
-        d2u_2 += u(i*h)*a2[i+2]
+    for i,a in zip(range(-2, 3),a2):
+        d2u_2 += u(x+i*h)*a
     
     return d2u_2
 
-print("Backward:", d2u_1(0.001))
-print("Centered", d2u_2(0.001))
+print("Exact value:",exact_value)
+print("Backward:", d2u_b(0.001))
+print("Centered:", d2u_c(0.001))
 
-#%% Exercise d
-
-# Range of h list 
-range_s = 20-2
-# Computing the values of h for different values of s
-h_list = [1/(2**s) for s in range(2,20)]
-
-# Initializing error lists
-error1 = []
-error2 = []
-
-# Computing the errors
-for i in range(range_s):
-    error1.append(abs(exact_value - d2u_1(h_list[i])))
-    error2.append(abs(exact_value - d2u_2(h_list[i])))
-
-# Convergence rates 
-CR_1 = 4
-CR_2 = 4
-
-# Making subplot
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 10))
-#ax1.loglog(h_list, error1, "-o")
-ax1.plot(np.log10(h_list),np.log10(error1),"-o")
-ax1.plot(np.log10(h_list),CR_1*np.log10(h_list))
-ax1.set_title('Backward operator')  
-ax1.set_xlabel('h')
-ax1.set_ylabel('d2u(h)') 
-#ax2.loglog(h_list, error2, "-o")
-ax2.plot(np.log10(h_list),np.log10(error2),"-o")
-ax2.plot(np.log10(h_list),CR_2*np.log10(h_list))
-ax2.set_title('Centered operator')
-ax2.set_xlabel('h')
-ax2.set_ylabel('d2u(h)')  
-
-plt.tight_layout()
-
-#plt.show()
-
-#%% Problem e
+#%% Checking order
 
 h = 0.1
 a_forward = np.array([35/12, -104/12, 114/12,-56/12, 11/12])/(h**2)
@@ -93,6 +61,38 @@ print("Forword:",[C(i,a_forward,0,4) for i in range(7)])
 print("Backword:",[C(i,a_backward,4,0) for i in range(7)])
 print("Centered:",[C(i,a_centered,2,2) for i in range(7)])
 
+#%% Exercise d
+
+# Computing the values of h for different values of s
+h_list = [1/(2**s) for s in range(2,20)]
+
+# Initializing error lists
+error_backword = [abs(exact_value - d2u_b(h_list[i])) for i in range(len(h_list))]
+error_centered = [abs(exact_value - d2u_c(h_list[i])) for i in range(len(h_list))]
+
+# Convergence rates 
+CR_backword = 3
+CR_centered = 4
+
+# Making subplot
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 10))
+#ax1.loglog(h_list, error1, "-o")
+ax1.plot(np.log10(h_list),np.log10(error_backword),"-o")
+ax1.plot(np.log10(h_list),CR_backword*np.log10(h_list))
+ax1.plot(np.log10(h_list),CR_centered*np.log10(h_list))
+ax1.set_title('Backward operator')  
+ax1.set_xlabel(r'$\log(h)$')
+ax1.set_ylabel(r'$\log(\Vert \hat{u} - u \Vert )$') 
+#ax2.loglog(h_list, error2, "-o")
+ax2.plot(np.log10(h_list),np.log10(error_centered),"-o")
+ax2.plot(np.log10(h_list),CR_centered*np.log10(h_list))
+ax2.set_title('Centered operator')
+ax2.set_xlabel(r'$\log(h)$')
+ax2.set_ylabel(r'$\log(\Vert \hat{u} - u \Vert) $')  
+
+plt.tight_layout()
+
+plt.show()
 
 
 # %%
