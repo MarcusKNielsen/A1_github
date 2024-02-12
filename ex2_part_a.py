3402e7dfc78d9af09940ec2e59d64de486707182
Max <mkn.net@gmail.com>
2024-02-12 15:05:02 +0100

    fandt en fejl i det nye scripy, som nu er rettet

--- ex2_part_a.py
+++ ex2_part_a.py
@@ -6,13 +6,12 @@ The implementation is a ajusted version of the one describe in section
 page 38 in the book (Finite Difference Methods-Leveque)
 """
 
-
 import numpy as np
 from numpy.linalg import solve, norm
 import matplotlib.pyplot as plt
 
 # Define uniform grid/mesh
-m = 200
+m = 100
 x = np.linspace(0,1,m+2)
 h = 1/(m+1)
 
@@ -26,7 +25,6 @@ eps = 0.01
 
 #%%
 
-# This is the function 
 def res(U):
     G = np.zeros(m)
     for j in range(1,m+1):
@@ -40,15 +38,17 @@ def res(U):
 def Jac(U):
     # Construct Jacobian
     J = np.zeros([m,m])
-    for i in range(m):
+    for i in range(1,m+1):
         
-        if i != 0:
-            J[i,i-1] = eps/h**2 - U[i]/(2*h)
-            
-        J[i,i] = -2*eps/h**2 + (U[i+1] - U[i-1])/(2*h) - 1
+        if i != 1: # Lower diagonal
+            J[i-1,i-2] = eps/h**2 - U[i]/(2*h)
+        
+        # Diagonal
+        J[i-1,i-1] = -2*eps/h**2 + (U[i+1] - U[i-1])/(2*h) - 1
     
-        if i != m-1:
-            J[i,i+1] = eps/h**2 + U[i]/(2*h)
+        if i != m: # Upper diagonal
+            J[i-1,i] = eps/h**2 + U[i]/(2*h)
+            print(i)
     return J
 
 #%%
@@ -73,19 +73,17 @@ while r > tol and k < MaxIter:
     J = Jac(U)
     delta = solve(J,-G)
     
-    # Save old U to calculate residual
-    Uold = np.copy(U)
-    
     # Update U
     U[1:-1] += delta
     
     # Calculate residual
-    r = norm(U-Uold,np.inf)
+    r = norm(delta,np.inf)
     
-    #
+    # Update iteration counter
     k += 1
 
 #%%
+
 plt.figure()
 plt.plot(x,U)
 plt.xlabel("x")
