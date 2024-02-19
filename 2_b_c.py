f6e58df355032f95a7184a18e41a272b936db9bd
Marianadesamadsen <s201205@student.dtu.dk>
2024-02-19 12:51:39 +0100

    b side

--- 2_b_c.py
+++ 2_b_c.py
@@ -1,11 +1,14 @@
 import numpy as np 
 
+def exactfunc(x,y):
+    return np.sin(4*np.pi*(x+y))+np.cos(4*np.pi*x*y)
 
 def mat_A(m):
 
     h = 1/(m+1)
 
     A   = np.zeros([m*m,m*m])
+    b   = np.zeros(m*m)
     k   = 0
 
     for j in range(m*m):
@@ -21,8 +24,14 @@ def mat_A(m):
 
                 if k > 0:
                     row[k-1] = 1
+                if k <= 0:
+                    b[k] = h**2*exactfunc(x=j*h,y=i*h)
+                    print('yes')
                 if k >= m:
                     row[k-m] = 1
+                if k < m:
+                    b[k] = h**2*exactfunc(x=j*h,y=i*h)
+                    print('yes')
 
             # Inserting the current row
             A[j,:] = row
@@ -30,22 +39,17 @@ def mat_A(m):
         # Making symmetric
         A = np.tril(A)+np.triu(A.T,1)
 
-    return A
+    return A,b
 
-A = mat_A(3)  
-print(mat_A(3))  
-
-def exactfunc(x,y):
-    return np.sin(4*np.pi*(x+y))+np.cos(4*np.pi*x*y)
+A,b = mat_A(3)  
+print(A)
+print(b)  
 
 m = 5
 x = np.linspace(0,1,m+2)
 y = np.linspace(0,1,m+2)
 boundary = np.zeros([m+2,m+2])
 
-for i in x:
-    for j in y:
-        boundary[i,j] = 
 
 
 
