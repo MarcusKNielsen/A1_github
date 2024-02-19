e01ba175b9922ea679e69b6a0402dd0421d7c0f5
Marianadesamadsen <s201205@student.dtu.dk>
2024-02-19 16:18:43 +0100

    b vector

--- 2_b_c.py
+++ 2_b_c.py
@@ -8,7 +8,6 @@ def mat_A(m):
     h = 1/(m+1)
 
     A   = np.zeros([m*m,m*m])
-    b   = np.zeros(m*m)
     k   = 0
 
     for j in range(m*m):
@@ -24,14 +23,9 @@ def mat_A(m):
 
                 if k > 0:
                     row[k-1] = 1
-                if k <= 0:
-                    b[k] = h**2*exactfunc(x=j*h,y=i*h)
-                    print('yes')
+
                 if k >= m:
                     row[k-m] = 1
-                if k < m:
-                    b[k] = h**2*exactfunc(x=j*h,y=i*h)
-                    print('yes')
 
             # Inserting the current row
             A[j,:] = row
@@ -39,16 +33,51 @@ def mat_A(m):
         # Making symmetric
         A = np.tril(A)+np.triu(A.T,1)
 
-    return A,b
+    return A
 
-A,b = mat_A(3)  
-print(A)
-print(b)  
+def vec_b(m):
+
+    h = 1/(m+1)
+    
+    x = np.linspace(0,1,m+2)
+    y = np.linspace(0,1,m+2)
+    b = np.zeros(m*m)
+
+    for j in range(m):
+        for i in range(m):
+
+            k = i + j*m
+
+            # The corners
+            if k == 0: # left bottom corner
+                b[k] = -exactfunc(x[j],y[i+1])/h**2 - exactfunc(x[j+1],y[i])/h**2  
+            elif k == m-1: # right bottem corner
+                b[k] = -exactfunc(x[j+2],y[i+1])/h**2 - exactfunc(x[j+1],y[i])/h**2  
+            elif k == m**2-m: # left upper corner
+                b[k] = -exactfunc(x[j],y[i+1])/h**2 - exactfunc(x[j+1],y[i+2])/h**2 
+            elif k == m**2-1: # right upper corner
+                b[k] = -exactfunc(x[j+2],y[i+2])/h**2 - exactfunc(x[j+1],y[i+2])/h**2  
+            elif k > 0 and k < (m-1): # bottem row
+                b[k] = -exactfunc(x[j+1],y[i])/h**2
+            elif k > m**2-m and k < m**2-1: # upper row
+                b[k] = -exactfunc(x[j+1],y[i+2])/h**2  
+
+            # These are checked after the first if loop
+            # left column
+            if k%m == 0 and b[k] == 0:
+                b[k] = -exactfunc(x[j-1],y[i])/h**2 
+            # right column
+            if (k+1)%m and b[k] == 0: 
+                b[k] = -exactfunc(x[j+1],y[i])/h**2
+
+    return b
 
 m = 5
-x = np.linspace(0,1,m+2)
-y = np.linspace(0,1,m+2)
-boundary = np.zeros([m+2,m+2])
+A = mat_A(m)  
+print(A) 
+b = vec_b(m)
+print(b)
+
 
 
 
