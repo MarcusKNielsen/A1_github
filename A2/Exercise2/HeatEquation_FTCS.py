2ae9b8b28f5043ceee63e85f7c830c138094eb22
Max <mkn.net@gmail.com>
2024-04-22 06:41:55 +0200

    some code

---
+++ A2/Exercise2/HeatEquation_FTCS.py
@@ -0,0 +1,108 @@
+import numpy as np
+from scipy.sparse import diags
+
+eps = 0.1
+
+def u_exact(x,t):
+    term1 = np.exp(-eps *  1**2 * t) * np.cos( 1*x)
+    term2 = np.exp(-eps *  4**2 * t) * np.cos( 4*x)
+    term3 = np.exp(-eps * 16**2 * t) * np.cos(16*x)
+    return term1 + term2 + term3
+
+# Define the size of your matrix
+m = 2**8  # Replace with the actual size of your matrix
+
+# Define the main diagonal
+main_diag = -2 * np.ones(m)
+
+# Define the off diagonals
+off_diag = np.ones(m - 1)
+
+# Create the diagonals
+diagonals = [off_diag,main_diag, off_diag]
+
+# Create A matrix
+h = 1/(m+1)
+A = diags(diagonals, [-1, 0, 1], format='csr') / (h**2)
+
+k = h**2
+
+valid_scheme = (k*eps/h**2 <= 0.5)
+print(valid_scheme)
+
+def forward(U,g):
+    return U+eps*k*A@U + eps*k*g
+
+x = np.linspace(-1,1,m+2)
+U = u_exact(x[1:-1],0).reshape((m, 1))
+g = np.zeros([m,1])
+
+T = 0.1
+t = 0
+
+Tarr = np.zeros(int(T/k)+2)
+Uarr = np.zeros([int(T/k)+2,m])
+Uarr[0,:] = U.ravel()
+i = 1
+
+#%%
+
+while t < T:
+    
+    # Update boundary
+    g[0]  = u_exact(-1,t) / (h**2)
+    g[-1] = u_exact( 1,t) / (h**2)
+    
+    # Update solution
+    U = forward(U,g)
+    Uarr[i,:] = U.ravel()
+    
+    # update time
+    t += k
+    Tarr[i] = t
+
+    # update counter
+    i += 1
+    
+
+#%%
+
+import matplotlib.pyplot as plt
+
+u = Uarr
+
+X,T_mesh = np.meshgrid(x[1:-1],Tarr)
+
+uexact = u_exact(X,T_mesh)
+
+err = uexact-u
+
+fig, ax = plt.subplots(1, 3, figsize=(10, 4))
+
+cbar_fraction = 0.05
+
+ax0 = ax[0].pcolormesh(X, T_mesh, uexact)
+ax[0].set_title("Exact Solution")
+#ax[0].set_aspect('equal', 'box')
+ax[0].set_xlabel("x")
+ax[0].set_ylabel("t")
+fig.colorbar(ax0, ax=ax[0], fraction=cbar_fraction)
+
+ax1 = ax[1].pcolormesh(X, T_mesh, u)
+ax[1].set_title("Numerical Solution")
+#ax[1].set_aspect('equal', 'box')
+ax[1].set_xlabel("x")
+ax[1].set_ylabel("t")
+fig.colorbar(ax1, ax=ax[1], fraction=cbar_fraction)
+
+ax2 = ax[2].pcolormesh(X, T_mesh, err)
+ax[2].set_title("Error")
+#ax[2].set_aspect('equal', 'box')
+ax[2].set_xlabel("x")
+ax[2].set_ylabel("t")
+fig.colorbar(ax2, ax=ax[2], fraction=cbar_fraction)
+
+fig.subplots_adjust(wspace=0.4)
+
+#%%
+
---
+++ A2/Exercise3/plot_of_r_stability.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+m = 100
+
+def r(p,c):
+    return np.sqrt(( 1-c )**2 + c**2 + 2*( 1-c )*c*np.cos(2*np.pi*p/(m+1)))
+
+p = np.linspace(1,m+1,m+1)
+
+plt.figure()
+plt.plot(p,r(p,0.2),label="c=0.2")
+plt.plot(p,r(p,0.5),label="c=0.5")
+plt.plot(p,r(p,0.7),label="c=0.7")
+plt.plot(p,r(p,1.0),label="c=1.0")
+plt.plot(p,r(p,1.2),label="c=1.2")
+plt.plot(p,r(p,1.5),label="c=1.5")
+plt.xlabel("p")
+plt.ylabel("r(p,c)")
+plt.legend()
+plt.show()
+
