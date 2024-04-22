40c6dad96c670fa651a84b065dae3c6c6b7a32d0
Marianadesamadsen <s201205@student.dtu.dk>
2024-04-22 13:05:20 +0200

    plots

--- A2/Exercise2/HeatEquation_FTCS_convergence.py
+++ A2/Exercise2/HeatEquation_FTCS_convergence.py
@@ -1,91 +1,30 @@
 import numpy as np
 from scipy.sparse import diags
+from HeatEquation_FTCS import*
+import matplotlib.pyplot as plt
 
 eps = 0.1
-
-def u_exact(x,t):
-    term1 = np.exp(-eps *  1**2 * t) * np.cos( 1*x)
-    term2 = np.exp(-eps *  4**2 * t) * np.cos( 4*x)
-    term3 = np.exp(-eps * 16**2 * t) * np.cos(16*x)
-    return term1 + term2 + term3
-
-
-def solve(m):
-    # Define the main diagonal
-    main_diag = -2 * np.ones(m)
-    
-    # Define the off diagonals
-    off_diag = np.ones(m - 1)
-    
-    # Create the diagonals
-    diagonals = [off_diag,main_diag, off_diag]
-    
-    # Create A matrix
-    h = 1/(m+1)
-    A = diags(diagonals, [-1, 0, 1], format='csr') / (h**2)
-    
-    
-    k = h**2
-    
-    valid_scheme = (k*eps/h**2 <= 0.5)
-    print(valid_scheme)
-    
-    def forward(U,g):
-        I = np.eye(m)
-        return (I+eps*k*A) @ U + eps*k*g
-    
-    x = np.linspace(-1,1,m+2)
-    U = u_exact(x[1:-1],0).reshape((m, 1))
-    g = np.zeros([m,1])
-    
-    T = 0.1
-    t = 0
-    
-    Tarr = np.zeros(int(T/k)+2)
-    Uarr = np.zeros([int(T/k)+2,m])
-    Uarr[0,:] = U.ravel()
-    i = 1
-    
-    
-    while t < T:
-        
-        # Update boundary
-        g[0] = u_exact(-1,t)/ (h**2)
-        g[-1] = u_exact(1,t)/ (h**2)
-        
-        # Update solution
-        U = forward(U,g)
-        Uarr[i,:] = U.ravel()
-        
-        # update time
-        t += k
-        Tarr[i] = t
-    
-        # update counter
-        i += 1
-        
-        
-    X,T_mesh = np.meshgrid(x[1:-1],Tarr)
-    
-    uexact = u_exact(X,T_mesh)
-    
-    err = np.max(np.abs(uexact-Uarr))
-
-    return err
-
-
 E = []
-s = np.arange(2,7)
 H = []
-for i in range(len(s)):
-    m = 2**s[i] - 1
+
+s = np.arange(2,7)
+for s_i in s:
+
+    m = 2**s_i - 1
     h = 1/(m+1)
-    E.append(solve(m))
+
+    Tarr, Uarr, x = solve_diffusion(m,eps)
+    err = solution_check(Tarr, Uarr, x, eps, False)
+
+    E.append(np.max(np.abs(err)))
     H.append(h)
 
-
-import matplotlib.pyplot as plt
-plt.plot(np.log(H),np.log(E),"o-")
+a,b = np.polyfit(np.log(H),np.log(E),1)
+plt.figure()
+plt.plot(np.log(H),np.log(E),"o-","b",label="convergence test")
+plt.plot(np.log(H),a*np.log(H)+b,"-","r",label="Helper line of order 2")
+print(a)
+plt.legend()
 plt.show()
 
 
